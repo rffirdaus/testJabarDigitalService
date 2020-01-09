83380133045120c06490c2477e8df797af06cea0
Firdaus <rff.firdaus@gmail.com>
2020-01-09 11:11:13 +0700

    revisi

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav" class="d-flex justify-content-center">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "./src/styles/global.scss";
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="col-5">
+    <div class="col-12">
+      <h5 class="text-center m-top">Pendaftaran Online</h5>
+      <div>
+        <div>Nama</div>
+        <input type="text" placeholder="Nama" class="form-control" />
+        <div>Tanggal Lahir</div>
+        <input type="date" placeholder="Tanggal Lahir" class="form-control" />
+        <div>Jenis kelamin</div>
+        <b-form-input list="my-list-id"></b-form-input>
+        <datalist id="my-list-id">
+          <option v-for="kelamin in kelamins" :key="kelamin">{{kelamin}}</option>
+        </datalist>
+        <div>Alamat</div>
+        <textarea placeholder="Alamat" class="form-control"></textarea>
+        <div>Daftar Riwayat Hidup</div>
+        <textarea placeholder="Riwayat Hidup" class="form-control"></textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      kelamins: ["Pria", "Wanita"]
+    };
+  }
+};
+</script>
+
+<style>
+.m-top {
+  margin-top: 20px;
+}
+</style>
